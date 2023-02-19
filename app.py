2ba87a8261f6e45fd47ec0450aac932ddd290a7c
CaiLun <112481366+CaiLunWi@users.noreply.github.com>
2023-02-19 07:23:49 +0000

    render dynamic data using templates tags

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+JOBS = [
+  {
+    'job_id': 1,
+    'title': 'Barista',
+    'location': 'Indonesia, Jakarta',
+    'salary': '4000 yuan'
+  },
+  {
+    'job_id': 2,
+    'title': 'Cafeteria Cook',
+    'location': 'Indonesia, Jakarta',
+    'salary': '3800 yuan'
+  },
+  {
+    'job_id': 3,
+    'title': 'Cafeteria Worker',
+    'location': 'Indonesia, Jakarta',
+    'salary': '3500 yuan'
+  },
+  {
+    'job_id': 4,
+    'title': 'Cafe Attendant',
+    'location': 'Indonesia, Jakarta',
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS, company_name='Sangrai Kopi')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
