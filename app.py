ea7876e68bc53a2e89d6ee8860ee55136c39ed30
CaiLun <112481366+CaiLunWi@users.noreply.github.com>
2023-02-28 05:10:37 +0000

    added dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -12,6 +12,13 @@ def hello_world():
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(jobs)
+
+@app.route("/jobs/<job_id>")
+def show_job(job_id):
+  job = load_job_from_db(job_id)
+  if not job:
+    return "Not Found"
+  return render_template("jobpage.html", job=job)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -12,11 +12,26 @@ engine = create_engine(
   })
 
 def load_jobs_from_db():
-  with engine.connect() as con:
-    result = con.execute(text("select * from jobs"))
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
     jobs = []
     for row in result.all():
       row_as_dict = row._mapping
       jobs.append(dict(row_as_dict))
   return jobs
+
+def load_job_from_db(job_id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE job_id = :job_id"),         
+    dict(job_id=job_id))
+    rows = result.all() 
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0]._mapping)
+
+        
+        
+    
   
