7af079eb09a1c0e054343bf397c3161882dcd46b
CaiLun <20705017-CaiLun@users.noreply.replit.com>
2023-11-28 20:42:57 +0000

    added a lot of functions, login, register, page for coffee items, redesign of previous pages

--- app.py
+++ app.py
@@ -1,13 +1,80 @@
-from flask import Flask, render_template, jsonify, request
-from database import load_jobs_from_db, load_job_from_db, add_application_to_db
+from flask import Flask, render_template, jsonify, request, flash
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db, register_user, load_users_from_db, get_user_by_id, get_user_by_username, load_coffee_from_db
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my_secret_key'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 jobs = load_jobs_from_db()
+users = load_users_from_db()
+coffee = load_coffee_from_db()
+
+class User(UserMixin):
+  def __init__(self, user_id, username, email, user_role):
+      self.id = user_id
+      self.username = username
+      self.email = email
+      self.user_role = user_role
+
+@login_manager.user_loader
+def load_user(user_id):
+  # Load user from the database based on user_id
+  user_data = get_user_by_id(user_id)
+  if user_data:
+      return User(user_data['user_id'], user_data['username'], user_data['email'], user_data['user_role'])
+  return None
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Validate user credentials
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user_data = get_user_by_username(username)
+        if user_data and user_data['password'] == password:
+            user = User(user_data['user_id'], user_data['username'], user_data['email'], user_data['user_role'])
+            login_user(user)
+            flash('Login successful!', 'success')
+            return render_template('dashboard.html', user=current_user)
+
+    return render_template('login.html')
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    print("Current User:", current_user)
+    return render_template('dashboard.html', user=current_user)
+
+@app.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  flash('Logout successful!', 'success')
+  return render_template('login.html')
 
 @app.route("/")
 def hello_world():
   return render_template("home.html", jobs=jobs, company_name='Sangrai Kopi')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+  
+@app.route('/career')
+def career():
+    return render_template('career.html', jobs=jobs)
+
+@app.route('/coffee')
+def coffee_page():
+    return render_template('coffee.html', coffee=coffee)
 
 @app.route("/api/job/<job_id>")
 def show_job_json(job_id):
@@ -28,6 +95,13 @@ def apply_for_job(job_id):
   add_application_to_db(job_id, data)
   return render_template('application_submitted.html', application=data, job=job)
   
+
+@app.route('/register', methods=['post'])
+def register_user_route():
+  data = request.form
+  register_user(data)
+  return render_template('registration_success.html')
+        
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, text, bindparam
+from sqlalchemy import create_engine, text
 import os
 
 connection_string = os.environ['CONNECTION_STRING']
@@ -12,13 +12,23 @@ engine = create_engine(
   })
 
 def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs = []
-    for row in result.all():
-      row_as_dict = row._mapping
-      jobs.append(dict(row_as_dict))
-  return jobs
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = []
+        for row in result.all():
+            row_as_dict = row._mapping
+            jobs.append(dict(row_as_dict))
+    return jobs
+
+def load_users_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from users"))
+        users = []
+        for row in result.all():
+            row_as_dict = row._mapping
+            users.append(dict(row_as_dict))
+    return users
+
 
 def load_job_from_db(job_id):
   with engine.connect() as conn:
@@ -45,7 +55,48 @@ def add_application_to_db(id, data):
                  [
                    {"job_id": job_id, "full_name": full_name, "email": email, "linkedin_url": linkedin_url, "education": education, "work_experience": work_experience, "resume_url": resume_url}
                  ])
-    
+
+def register_user(data):
+  with engine.connect() as conn:
+      # Insert the user information into the Users table
+      query = text("INSERT INTO users (username, email, password, user_role) VALUES (:username, :email, :password, :user_role)")
+      username = data['username']
+      email = data['email']
+      password = data['password']
+      user_role = '0'
+      conn.execute(query,
+                   [
+                       {"username": username, "email": email, "password": password, "user_role": user_role}
+                   ])
+
+def get_user_by_id(user_id):
+  with engine.connect() as conn:
+      result = conn.execute(text("SELECT * FROM users WHERE user_id =         :user_id"), dict(user_id=user_id))
+      rows = result.all() 
+      if len(rows) == 0:
+        return None
+      else:
+        return dict(rows[0]._mapping)
+
+def get_user_by_username(username):
+  with engine.connect() as conn:
+      result = conn.execute(
+      text("SELECT * FROM users WHERE username = :username"),       
+      dict(username=username))
+      rows = result.all() 
+      if len(rows) == 0:
+        return None
+      else:
+        return dict(rows[0]._mapping)
+
+def load_coffee_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from coffee"))
+    coffee = []
+    for row in result.all():
+        row_as_dict = row._mapping
+        coffee.append(dict(row_as_dict))
+    return coffee
     
 
         
